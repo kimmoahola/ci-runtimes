9cb16e9c94e70bce00a059203bf9577ad43ea05e
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2021-09-15 09:46:53 +0300

    Add LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ci-runtimes "0.1.0"
   :description "List Github actions runtimes"
   :url "https://github.com/kimmoahola/ci-runtimes"
-  :license {:name "MIT"
-            :url "https://mit-license.org/"}
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.12.1"]
                  [cheshire "5.10.0"]]
